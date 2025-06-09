b298a04b98005a0a06e9392bfba5fb755431d82f
André Bernardes <andrebernardes.midias@gmail.com>
2025-06-09 16:41:42 -0400

    Delete backend directory
    limpando

--- backend/routes/membroRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const membroController = require('../controllers/membroController');
-
-router.get('/', membroController.listarMembros);
-router.post('/', membroController.criarMembro);
-
-module.exports = router;
